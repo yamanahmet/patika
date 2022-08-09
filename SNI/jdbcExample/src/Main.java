9f52842f560b3ea431a93680c2b07083ba0ff71e
Ahmet YAMAN <ymna32@gmail.com>
2022-08-09 17:50:28 +0300

    JDBS CRUD

--- SNI/jdbcExample/src/Main.java
+++ SNI/jdbcExample/src/Main.java
@@ -1,10 +1,18 @@
 import dao.TraineeDAO;
+import entity.Trainee;
 
 import java.sql.*;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
         TraineeDAO traineeDAO = new TraineeDAO();
         traineeDAO.createTrainee();
+        //traineeDAO.traineeUpdate(2,"Mehmet", 27);
+        //traineeDAO.traineeDelete(2);
+        for (Trainee i : traineeDAO.traineeRead()
+             ) {
+            System.out.println(i.getId() + " " + i.getName() + " " + i.getAge());
+        }
     }
 }
--- SNI/jdbcExample/src/dao/TraineeDAO.java
+++ SNI/jdbcExample/src/dao/TraineeDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
+import entity.Trainee;
 import util.DBConnection;
 
-import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TraineeDAO {
 
@@ -25,19 +27,96 @@ public class TraineeDAO {
             Statement statement = this.getDb().createStatement();
 
             // prepare query
-            String query =
+            /*String query =
                     "CREATE TABLE trainee (" +
-                            "id serial primary key," +
+                    "id serial primary key," +
                     "name varchar(255)," +
                     "age Integer)";
 
-            statement.executeQuery(query);
+            statement.executeQuery(query); */
 
+            String insertQuery =
+                    "INSERT INTO trainee(id, name, age) VALUES (1, 'Ahmet', 25);"+
+                    "INSERT INTO trainee(id, name, age) VALUES (2,'Ozan', 22);";
+
+            statement.executeQuery(insertQuery);
 
 
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
+
+    public void traineeUpdate(int id, String name, int age){
+
+        String updateQuery = "UPDATE trainee SET" +
+                " name = ?," +
+                " age = ?" +
+                " WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = this.getDb().prepareStatement(updateQuery)) {
+            preparedStatement.setInt(3,id);
+            preparedStatement.setInt(2,age);
+            preparedStatement.setString(1,name);
+
+            int rows =  preparedStatement.executeUpdate();
+            if (rows > 0){
+                System.out.println("rows " + rows);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());;
+        }
+
+    }
+
+    public void traineeDelete(int id){
+        String deleteQuery = "DELETE FROM trainee" +
+                " WHERE id = ?";
+        try (PreparedStatement preparedStatement = this.getDb().prepareStatement(deleteQuery)){
+            preparedStatement.setInt(1,id);
+            preparedStatement.execute();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
+
+    public List<Trainee> traineeRead(){
+        String readQuery = "SELECT * FROM trainee";
+
+        // statement
+        Statement statement = null;
+
+        int id, age;
+        String name;
+        List<Trainee> traineeList = new ArrayList<>();
+
+        try {
+            statement = this.getDb().createStatement();
+            // execute query
+            ResultSet resultSet = statement.executeQuery(readQuery);
+            // close
+            getDb().close();
+
+
+
+            while (resultSet.next()){
+
+                id =  resultSet.getInt("id");
+                name = resultSet.getString("name");
+                age =  resultSet.getInt("age");
+
+                traineeList.add(new Trainee(id, name, age));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return traineeList;
+
     }
 
 }
