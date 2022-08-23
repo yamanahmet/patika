30d1bec6532e3b96b8f8051eec313927dcc4e9d9
Ahmet YAMAN <ymna32@gmail.com>
2022-08-23 11:49:34 +0300

    js hw1

---
+++ kodluyoruz-frontend-101-egitimi/04-javascript-kategorisi/data/number.js
@@ -0,0 +1,12 @@
+let price = 100
+let tax = 0.18
+let priceTax = price * tax
+let total = price + priceTax
+console.log(
+    "Fiyat: ", price, 
+    "KDV Orani: ", tax, 
+    "KDV Tutari", priceTax, 
+    "Fiyat: ", total
+    )
+
+    ``
